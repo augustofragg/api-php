53fcb7fde09446177a1aef4fd6fe9fd788886fc0
Augusto fragg <augustofragg@gmail.com>
2024-12-26 15:58:49 -0300

    api implementation get

--- return.php
+++ return.php
@@ -2,10 +2,9 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-
-
-
+ 
 echo json_encode($array);
 
 exit;
